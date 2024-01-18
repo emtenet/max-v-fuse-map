f28988d12085f1fa7c7c4e0b45b63c28cefe7085
Michael Taylor <michael@emte.net.au>
2024-01-18 19:43:48 +1100

    Add package reset & output enable pins

--- experiments/src/package.erl
+++ experiments/src/package.erl
@@ -3,6 +3,8 @@
 -export([list/0]).
 -export([title/1]).
 -export([gclk_pins/1]).
+-export([reset/1]).
+-export([output_enable/1]).
 
 -export_type([package/0]).
 
@@ -64,4 +66,34 @@ gclk_pins(t100) -> [pin12, pin14, pin62, pin64];
 gclk_pins(t144) -> [pin18 ,pin20, pin89, pin91];
 gclk_pins(f256) -> [h5, j5, j12, h12];
 gclk_pins(f324) -> [j6, k6, k13, j13].
+
+%%====================================================================
+%% reset
+%%====================================================================
+
+-spec reset(package()) -> pin().
+
+reset(e64) -> pin29;
+reset(m64) -> h7;
+reset(m68) -> h7;
+reset(m100) -> k8;
+reset(t100) -> pin44;
+reset(t144) -> pin61;
+reset(f256) -> m9;
+reset(f324) -> n10.
+
+%%====================================================================
+%% output_enable
+%%====================================================================
+
+-spec output_enable(package()) -> pin().
+
+output_enable(e64) -> pin28;
+output_enable(m64) -> f6;
+output_enable(m68) -> j6;
+output_enable(m100) -> l8;
+output_enable(t100) -> pin43;
+output_enable(t144) -> pin60;
+output_enable(f256) -> m8;
+output_enable(f324) -> n9.
 
